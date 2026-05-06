247bb04017187e1ece7cd61cb77fc660b71d246e
canelinha0222-design <canelinha0222@gmail.com>
2026-05-06 03:15:09 +0000

    Pending changes exported from your codespace

--- src/Questao5/Main.java
+++ src/Questao5/Main.java
@@ -1,17 +1,25 @@
 package Questao5;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-        ArrayList<String> animais = new ArrayList<>();
-
-        animais.add("Coelho");
-        animais.add("Lobo");
-        animais.add("Corvo");
-
-        animais.set(1, "Gato");
-        for (String animal : animais) {
-            System.out.println(" " + animal);
+    double numero = 0;
+    Scanner scanner = new Scanner(System.in);
+    numero = scanner.nextDouble();
+    for(int i = 1; i <= 5; i++){
+        System.out.println("Digite um valor para dividir por");
+        double numero2 = scanner.nextDouble();
+        try {
+            numero = numero / numero2;
+        if(Double.isNaN(numero) || Double.isInfinite(numero)){
+            throw new ArithmeticException("Erro");
         }
+    }catch(ArithmeticException e){
+        System.out.println("Divisão por zero " + e);
+    }
+        System.out.println("O resultado é " + numero);
+    }
+
     }
 }
